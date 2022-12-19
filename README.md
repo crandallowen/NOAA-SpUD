e4056bbfd114e7b27c48380470a2fb8882b3c54f
crandallowen <61884116+crandallowen@users.noreply.github.com>
2022-12-19 10:25:47 -0500

    Bug fixes in RFA.py and first sample script
    Fixed some issues in RFA.py, and a new format for .csv output.
    Created SXXI1ColToCSV.py which is the first sample driver script added which shows some basic usage of the module.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NOAA SpUD
- 
+A database used to track the spectrum usage of NOAA for the purpose of spectrum management.
+
+# RFA.py
+A python module that allows a user to more easily manipulate output data from SXXI. This module contains a class declaration for an RFA object, useful methods for manipulating RFA objects, a function to import records in either GMF or SFAF format, functions for exporting the data in particular formats, and functions for converting SXXI values into more readable formats.
---
+++ SXXI1ColToCSV.py
@@ -0,0 +1,21 @@
+import RFA as rfa
+
+# Prompt user for input file name and ensure correct file extension
+userIn = ''
+inFilename = ''
+acceptedInFileExtension = '.txt'
+while(userIn[-len(acceptedInFileExtension):] != acceptedInFileExtension):
+    userIn = input(f'Enter name of input file. File must end in {acceptedInFileExtension}.\n')
+inFilename = userIn
+
+# Prompt the user for output file name and ensure correct file extension
+outFilename = 'output.csv' # Default out file name if none selected. Technically, not necessary as out file name is optional for exportRFAsToCSV()
+acceptedOutFileExtension = '.csv'
+while(userIn[-len(acceptedOutFileExtension):] != acceptedOutFileExtension):
+    userIn = input(f'Enter name of output file. File must end in {acceptedOutFileExtension}. (ENTER for \'{outFilename}\')\n')
+    if userIn == '':
+        userIn = outFilename
+outFilename = userIn
+
+RFAs = rfa.importRFAsFrom1ColFile(inFilename)
+rfa.exportRFAsToCSV_NWSFormat(RFAs, outFilename)
