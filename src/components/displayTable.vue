40d98a92c4926bd0af11bcb011aba16aab8ea9c7
crandallowen <61884116+crandallowen@users.noreply.github.com>
2023-03-03 14:50:41 -0500

    Implemented sortable columns
    Implemented logic to sort columns with default sorting columns by frequency
    Added buttons in column headers to allow sorting by column that display current sort direction
    Changed default font
    Changed min-width of column select to better reflect the minimum needed width of the column
    Refactored some css
    Absorbed RFAHeaderMap.js into utils.js

--- src/components/displayTable.vue
+++ src/components/displayTable.vue
@@ -10,6 +10,37 @@ const props = defineProps({
 });
 
 const activeColumns = ref([...defaultColumns]);
+const sortField = ref('frequency_khz');
+const sortDirection = ref('ascending');
+
+const sortedRows = computed(() => {
+    let rows = [...props.rows];
+    if (sortField.value === 'frequency_khz') {
+        if (sortDirection.value === 'ascending')
+            return rows.sort((a,b) => a[sortField.value] - b[sortField.value]);
+        else
+            return rows.sort((a,b) => b[sortField.value] - a[sortField.value]);
+    } else {
+        if (sortDirection.value === 'ascending')
+            return rows.sort((a,b) => (a[sortField.value] > b[sortField.value]) ? 1 : ((b[sortField.value] > a[sortField.value]) ? -1 : 0));
+        else
+            return rows.sort((a,b) => (a[sortField.value] < b[sortField.value]) ? 1 : ((b[sortField.value] < a[sortField.value]) ? -1 : 0));
+    }
+});
+
+function sortByColumn(event) {
+    let column = event.srcElement.id;
+    if (column === sortField.value) {
+        if (sortDirection.value === 'ascending')
+            sortDirection.value = 'descending';
+        else
+            sortDirection.value = 'ascending';
+    } else {
+        sortField.value = column;
+        sortDirection.value = 'ascending';
+    }
+    console.log('Sort rows by', sortField.value, 'in', sortDirection.value, 'order');
+}
 </script>
 
 <template>
@@ -29,18 +60,21 @@ const activeColumns = ref([...defaultColumns]);
                 <thead>
                 <tr>
                     <template v-for="value in props.columns">
-                    <th v-show="activeColumns.includes(value)">
-                        {{ headerMap(value) }}
-                    </th>
+                        <th v-show="activeColumns.includes(value)">
+                            {{ headerMap(value) }}
+                            <button @click="sortByColumn" :id="value" class="sortButton">
+                                {{ sortField != value ? '^/v' : (sortDirection === 'ascending' ? '^' : 'v') }}
+                            </button>
+                        </th>
                     </template>
                 </tr>
                 </thead>
                 <tbody>
-                <tr v-for="row in props.rows">
+                <tr v-for="row in sortedRows">
                     <template v-for="(value, key) in row">
-                    <td v-show="activeColumns.includes(key)">
-                        {{ format(value, key) }}
-                    </td>
+                        <td v-show="activeColumns.includes(key)">
+                            <pre>{{ format(value, key) }}</pre>
+                        </td>
                     </template>
                 </tr>
                 </tbody>
@@ -54,50 +88,55 @@ const activeColumns = ref([...defaultColumns]);
     display: flex;
     flex-direction: row;
 }
+
+.tableContainer, .columnSelect{
+    display: flex;
+    flex-direction: column;
+    width: max-content;
+}
+
 .columnSelect {
-    display: flex;
-    flex-direction: column;
-    padding-right: 4px;
-    min-width: 20px;
+    min-width: 26ch;
 }
-.tableContainer {
-    display: flex;
-    flex-direction: column;
-}
-.displayTable, .displayTable :deep(*) {
+
+.displayTable, .displayTable :deep(td), .displayTable :deep(th) {
     border: 1px solid var(--color-border);
     border-collapse: collapse;
     
 }
-.displayTable :deep(*) {
-    width: fit-content;
-    padding: 2px 5px;
-}
-.displayTable thead :deep(*) {
+
+.displayTable :deep(td), .displayTable :deep(th) {
     text-align: left;
     vertical-align: middle;
+    width: fit-content;
+    padding: 3px 5px;
+}
+
+.displayTable :deep(th) {
     white-space: nowrap;
     background-color: var(--color-background-soft);
     font-weight: bold;
 }
 
-.displayTable tbody :deep(*) {
-    text-align: left;
-    vertical-align: middle;
+/* .displayTable :deep(td) {
+    
+} */
 
+.inputLine :deep(label) {
+    color: var(--color-text);
+    white-space: nowrap;
+    padding: 0 5px;
 }
 
-button {
+.sortButton {
+    float: right;
+    background-color: var(--color-background-soft);
     border: 1px solid var(--color-border);
     color: var(--color-text);
     border-radius: 4px;
 }
 
-.activeColumn {
-    background-color: var(--color-toggle-active);
-}
-
-.inactiveColumn {
-    background-color: var(--color-background-soft);
+pre {
+    font-family: inherit;
 }
 </style>
